6fcdf601066009cc06b7ef7e7da8bb582e94a7ef
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:29:13 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP1_8_B/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ITP1_8_B
+{
+    class Program
+    {
+        static void Main()
+        {
+            while(true)
+            {
+                string Input = Console.ReadLine();
+                if (Input == "0") break;
+                int ans = 0;
+                foreach(char num in Input)
+                {
+                    int integer = (int)Char.GetNumericValue(num);
+                    ans += integer;
+                }
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
